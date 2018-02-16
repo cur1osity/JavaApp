bb61bf3cb82a729e8d4ad954317ba3bfd2b783bb
cur1osity <marcin.gajc@gmail.com>
2018-02-16 13:46:03 +0100

    Zadanie 24.2 v.1.0

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -11,6 +11,9 @@ public class AdminConfig {
     @Value ("${admin.mail}")
     private String adminMail;
 
+    @Value ("${admin.name}")
+    private String adminName;
+
     @Value("${second.mail}")
     private String secondEmail;
 }
